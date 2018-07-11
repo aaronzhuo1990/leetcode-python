2849c07373a8d8a1e4a191f4dd2a56eb229dcfb0
aaronzhuo1990 <aaronchuo1990@gmail.com>
2018-07-10 22:41:42 -0600

    Add the solution for 80: remove duplicates from sorted array

---
+++ 80_remove_duplicates_from_sorted_array/solution.py
@@ -0,0 +1,42 @@
+"""
+Problem:
+    https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/
+
+Example:
+    Given nums = [1,1,1,2,2,3],
+    Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+    It doesn't matter what you leave beyond the returned length.
+"""
+
+
+
+class Solution(object):
+    def remove_duplicates(self, array):
+        """
+        :param array: list[int]: sorted array with duplicates
+        :return: list[int]: the array without duplicates, int: length of the array
+        """
+        n = len(array)
+
+        if n == 0:
+            return [], 0
+
+        cursor = 0
+        duplicated = False
+
+        for i in range(1, n):
+            if array[cursor] == array[i] and not duplicated:
+                cursor += 1
+                array[cursor] = array[i]
+                duplicated = True
+            elif array[cursor] != array[i]:
+                cursor += 1
+                array[cursor] = array[i]
+                if duplicated:
+                    duplicated = False
+
+        return array[:cursor+1], cursor+1
+
+
+s = Solution()
+print s.remove_duplicates([0, 1, 1, 2, 2, 2, 3])
